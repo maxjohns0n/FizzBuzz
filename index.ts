b85e0cd585e7c3ed724d71e465903b4fb2b9991e
Max Johnson <max.johnson@softwire.com>
2024-04-24 17:02:20 +0100

    BANG

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
+function is_multiple_of(i: number, divisor: number) {
+    return i % divisor == 0;
+}
+
 function fizz(i: number) {
-    return i % 3 == 0;
+    return is_multiple_of(i, 3);
 }
 
 function buzz(i: number) {
-    return i % 5 == 0;
+    return is_multiple_of(i, 5);
+}
+
+function bang(i: number) {
+    return is_multiple_of(i, 7);
 }
 
 for (let i = 1; i <= 100; i++) {
@@ -13,6 +21,8 @@ for (let i = 1; i <= 100; i++) {
         result += "Fizz";
     if (buzz(i))
         result += "Buzz";
+    if (bang(i))
+        result += "Bang";
 
     if (result === "")
         result = i.toString();
