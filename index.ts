249900264c0aec84c438fc29736a43f7c9604fc1
Max Johnson <max.johnson@softwire.com>
2024-04-24 16:43:55 +0100

    Implement basic fizzbuzz

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("hello world")
+function fizz(i: number) {
+    return i % 3 == 0;
+}
+
+function buzz(i: number) {
+    return i % 5 == 0;
+}
+
+for (let i = 1; i <= 100; i++) {
+    let result = "";
+
+    if (fizz(i))
+        result += "Fizz";
+    if (buzz(i))
+        result += "Buzz";
+
+    if (result === "")
+        result = i.toString();
+
+    console.log(result);
+}
